70c9556cfcd7ce9b93f7fe8622b45f98e327f09f
Rogério Nóbrega <rogeriocnobrega@gmail.com>
2016-10-04 23:23:49 -0300

    Adicao da tela principal

--- Aula10/app/src/main/java/br/edu/ufcg/embedded/aula10/MainActivity.java
+++ Aula10/app/src/main/java/br/edu/ufcg/embedded/aula10/MainActivity.java
@@ -3,33 +3,30 @@ package br.edu.ufcg.embedded.aula10;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.login_email)
-    EditText mEmail;
-
-    @BindView(R.id.login_password)
-    EditText mPassword;
+    @BindView(R.id.contacts_list)
+    RecyclerView mContactRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
     }
 
-    @OnClick(R.id.btn_login)
-    public void onBtnLoginClicked(View v) {
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
+    @OnClick(R.id.btn_add_contact)
+    public void addContact(View view) {
+        startActivity(new Intent(this, ContactsActivity.class));
     }
+
 }
